9d11c423f50ce3ca544ba1cf4d08352c1c4e1c80
fergusch <27470183+fergusch@users.noreply.github.com>
2021-12-01 17:27:34 -0600

    auto download prompt + input

--- make_dir.py
+++ make_dir.py
@@ -1,9 +1,26 @@
 import sys
 import os
+import re
+import requests
+from bs4 import BeautifulSoup
+from markdownify import markdownify
+
+cookies = {'session': os.environ['ADVENT_SESSION']}
 
 year, day = sys.argv[1].split('/')
 os.mkdir(f'{year}/{day}')
-open(f'{year}/{day}/input.txt', 'w').close()
+
+r = requests.get(f'https://adventofcode.com/{year}/day/{int(day)}', cookies=cookies)
+soup = BeautifulSoup(r.text, 'html.parser')
+part1_html = soup.find('article').decode_contents()
+part1_html = re.sub('--- (.*) ---', r'\1', part1_html)
+with open(f'{year}/{day}/prompt.md', 'w') as f:
+    f.write(markdownify(part1_html).replace('\n\n', '\n'))
+
+r = requests.get(f'https://adventofcode.com/{year}/day/{int(day)}/input', cookies=cookies)
+with open(f'{year}/{day}/input.txt', 'w') as f:
+    f.write(r.text)
+
 with open(f'{year}/{day}/solution.py', 'w') as f:
     f.write(f'## advent of code {year}\n' + \
         f'## https://adventofcode.com/{year}\n' + \
