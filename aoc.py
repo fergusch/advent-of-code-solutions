92eeb9b869f58f17d0aae1674b76359384373b0e
fergusch <27470183+fergusch@users.noreply.github.com>
2021-12-02 22:29:01 -0600

    add private leaderboards

--- aoc.py
+++ aoc.py
@@ -3,11 +3,13 @@ import os
 import re
 import requests
 from importlib import import_module
+from datetime import datetime as dt
 from enum import Enum
 from bs4 import BeautifulSoup
 from markdownify import markdownify
 from termcolor import colored
 from tabulate import tabulate
+import pytz
 
 class Status(Enum):
     PASS = 0
@@ -73,8 +75,9 @@ if __name__ == '__main__':
         print('    - if both parts are solved, submit part 2.')
         print('    - if SOLUTION_FILE is specified and answers not yet submitted,')
         print('      submit answers using that file instead.\n')
-        print(colored('  aoc.py stats YEAR', 'magenta'))
-        print('    check progress and personal leaderboard stats for YEAR\n')
+        print(colored('  aoc.py stats YEAR [private]', 'magenta'))
+        print('    check progress and personal leaderboard stats for YEAR')
+        print('    - if "private" passed, show configured private leaderboard stats.\n')
         sys.exit(0)
 
     command = sys.argv[1]
@@ -84,25 +87,69 @@ if __name__ == '__main__':
 
     if command == 'stats':
 
-        r = requests.get(f'https://adventofcode.com/{year}/leaderboard/self', cookies=cookies)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        table = soup.select('article pre')[0].text
-        table_rows = [x.split() for x in table.split('\n')[2:-1]]
-        stars_per_day = [0]*25
-        for row in table_rows:
-            stars_per_day[int(row[0])-1] = 2 if row[4:7] != ['-', '-', '-'] else 1 if row[1:4] != ['-', '-', '-'] else 0
-        
-        print()
-        print(colored('(Part 1)', 'cyan'), colored('(Part 2)', 'yellow'))
-        print('\n         1111111111222222\n1234567890123456789012345')
-        for day in stars_per_day:
-            print(colored('*', 'yellow') if day == 2 else colored('*', 'cyan') if day == 1 else ' ', end='')
-        print(f" ({sum(stars_per_day)}{colored('*', 'yellow')})\n")
+        # I know this is a bad way to do this, but isn't that the fun of this whole thing?
+        if solution_file == 'private':
 
-        print(tabulate(table_rows, headers=['Day', 
-            *[colored(x, 'cyan') for x in ['Time', 'Rank', 'Score']],
-            *[colored(x, 'yellow') for x in ['Time', 'Rank', 'Score']]
-        ]), '\n')
+            private_leaderboards = os.environ['ADVENT_PRIV_BOARDS'].split(',')
+
+            for board_id in private_leaderboards:
+
+                r = requests.get(f'https://adventofcode.com/{year}/leaderboard/private/view/{board_id}', cookies=cookies)
+                soup = BeautifulSoup(r.text, 'html.parser')
+                board_owner = re.findall(r'private leaderboard of (.*) for', soup.select('article p')[0].text)[0]
+                rows = soup.find_all('div', class_='privboard-row')[1:]
+
+                top_score_len = len(rows[0].find_all(text=True, recursive=False)[0].strip())
+                print(f"\n{board_owner}'s private leaderboard {colored(f'({board_id})', 'grey')}")
+                print(f'\n{" "*(top_score_len+14)}1111111111222222\n{" "*(top_score_len+5)}1234567890123456789012345')
+
+                for i, row in enumerate(rows):
+                    position = row.find('span', class_='privboard-position').text
+                    stars = row.find_all('span', class_=re.compile('privboard-star-*'))
+                    name = row.find('span', class_='privboard-name').text
+                    name_link = row.select('.privboard-name a')[0].attrs['href'] if len(row.select('.privboard-name a')) else None
+                    score = row.find_all(text=True, recursive=False)[0].strip()
+
+                    print(f'{position} {score:>{top_score_len}}', end=' ')
+                    for i, span in enumerate(stars):
+                        class_ = span.attrs['class'][0]
+                        if 'both' in class_: 
+                            print(colored('*', 'yellow'), end='')
+                        elif 'firstonly' in class_:
+                            print(colored('*', 'cyan'), end='')
+                        elif 'unlocked' in class_:
+                            print(colored('*', 'grey'), end='')
+                        elif 'locked' in class_:
+                            print(' ', end='')
+                    
+                    print(f' {name}', end=' ')
+                    print(f'({colored(name_link, "blue")})' if name_link is not None else '')
+                
+                print()
+                print(colored('(1 star)', 'cyan'), colored('(2 stars)', 'yellow'), colored('(0 stars)', 'grey'), '\n')
+                
+
+        else:
+
+            r = requests.get(f'https://adventofcode.com/{year}/leaderboard/self', cookies=cookies)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            table = soup.select('article pre')[0].text
+            table_rows = [x.split() for x in table.split('\n')[2:-1]]
+            stars_per_day = [0]*25
+            for row in table_rows:
+                stars_per_day[int(row[0])-1] = 2 if row[4:7] != ['-', '-', '-'] else 1 if row[1:4] != ['-', '-', '-'] else 0
+            
+            print()
+            print(colored('(Part 1)', 'cyan'), colored('(Part 2)', 'yellow'))
+            print('\n         1111111111222222\n1234567890123456789012345')
+            for day in stars_per_day:
+                print(colored('*', 'yellow') if day == 2 else colored('*', 'cyan') if day == 1 else ' ', end='')
+            print(f" ({sum(stars_per_day)}{colored('*', 'yellow')})\n")
+
+            print(tabulate(table_rows, headers=['Day', 
+                *[colored(x, 'cyan') for x in ['Time', 'Rank', 'Score']],
+                *[colored(x, 'yellow') for x in ['Time', 'Rank', 'Score']]
+            ]), '\n')
     
     elif command == 'get':
         
